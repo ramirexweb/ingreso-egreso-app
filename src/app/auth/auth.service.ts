5877905f94ad9d9d885406c38dbe17cc2ac69248
ramirex <ramirex@gmail.com>
2020-02-14 15:00:42 -0400

    crear un documento para cada usuario

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,8 @@ import { map } from 'rxjs/operators';
 import * as firebase from 'firebase';
 
 import Swal from 'sweetalert2';
+import { User } from './user.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,8 @@ export class AuthService {
 
   constructor(
     private afAuth: AngularFireAuth,
-    private router: Router
+    private router: Router,
+    private afDB: AngularFirestore
   ) { }
 
   initAuthListener() {
@@ -36,7 +39,17 @@ export class AuthService {
   crearUsuario(nombre: string, email: string, password: string) {
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then( resp => {
-        this.router.navigate(['/']);
+
+        const user: User = {
+          uid: resp.user.uid,
+          nombre,
+          email: resp.user.email
+        };
+
+        this.afDB.doc(`${user.uid}/usuario`).set(user).then(() => {
+          this.router.navigate(['/']);
+        });
+
       }). catch( error => {
         console.error(error);
         Swal.fire('Error al crear el Usuario', error.message, 'error');

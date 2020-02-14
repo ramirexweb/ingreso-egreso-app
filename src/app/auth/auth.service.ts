f0a38793ff36f30911a6fa99eb1cac068fbddacf
ramirex <ramirex@gmail.com>
2020-02-14 12:08:13 -0400

    obtener informacion del uusario en firebase

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import * as firebase from 'firebase';
 
 import Swal from 'sweetalert2';
 
@@ -14,10 +15,15 @@ export class AuthService {
     private router: Router
   ) { }
 
+  initAuthListener() {
+    this.afAuth.authState.subscribe( (fbUser: firebase.User) => {
+      console.log(fbUser);
+    });
+  }
+
   crearUsuario(nombre: string, email: string, password: string) {
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then( resp => {
-        console.log(resp);
         this.router.navigate(['/']);
       }). catch( error => {
         console.error(error);
@@ -29,7 +35,6 @@ export class AuthService {
 
     this.afAuth.auth.signInWithEmailAndPassword(email, password)
       .then( resp => {
-        console.log(resp);
         this.router.navigate(['/']);
       }).catch( error => {
         console.log(error);

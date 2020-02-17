5d53c28a9af641353ad378d32a2744114c80376d
ramirex <ramirex@gmail.com>
2020-02-17 15:35:15 -0400

    MODELO DE INGRESO Y EGRESO

---
+++ src/app/ingreso-egreso/ingreso-egreso.model.ts
@@ -0,0 +1,21 @@
+export class IngrersoEgreso {
+  description: string;
+  monto: number;
+  tipo: string;
+  uid?: string;
+
+  constructor( obj: DataObj ) {
+
+    this.description = obj && obj.description || null;
+    this.monto       = obj && obj.monto       || null;
+    this.tipo        = obj && obj.tipo        || null;
+    this.uid         = obj && obj.uid         || null;
+  }
+}
+
+interface DataObj {
+  description: string;
+  monto: number;
+  tipo: string;
+  uid?: string;
+}

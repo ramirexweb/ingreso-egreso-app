caacb99e481d65188fa27d36a456a5b0d83c2210
ramirex <ramirex@gmail.com>
2020-02-17 20:39:36 -0400

    cargar datos desde firrebase

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IngresoEgresoService } from '../ingreso-egreso/ingreso-egreso.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ingresoEgresoService: IngresoEgresoService
+  ) { }
 
   ngOnInit(): void {
+    this.ingresoEgresoService.initIngresoEgresoListener();
   }
 
 }
--- src/app/ingreso-egreso/ingreso-egreso.service.ts
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { IngresoEgreso } from './ingreso-egreso.model';
 import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +13,22 @@ export class IngresoEgresoService {
 
   constructor(
     private afDB: AngularFirestore,
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<AppState>
   ) { }
+
+  initIngresoEgresoListener() {
+    this.store.select('auth').pipe(
+      filter( auth => auth.user !== null)
+    ).subscribe( auth => this.ingresoEgresoItems(auth.user.uid), err => console.log(err));
+  }
+
+  private ingresoEgresoItems( uid: string ) {
+    this.afDB.collection(`${uid}/ingresos-egresos/items`).valueChanges()
+      .subscribe( docData => {
+        console.log(docData);
+      });
+  }
 
   crearIngresoEgreso( ingresoEgreso: IngresoEgreso) {
     const user = this.authService.getUsuario();
